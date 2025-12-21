84f0941cfa2f8583fd0e31fe406181bc8bedd333
alchemist1111 <emoyoeugine@gmail.com>
2025-12-21 05:54:37 +0300

    Add swagger ui for api documentation

--- backend/short_url/urls.py
+++ backend/short_url/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .swagger import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('shortener.urls')),
+    
+    # Swagger UI
+    path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
